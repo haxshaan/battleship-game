5990673528d6d03114216575ab25ada37a36b76e
haxshaan <shantam.m22@gmail.com>
2017-06-18 12:57:50 +0530

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -1,20 +1,20 @@
-from random import randint
+from random import randint #import random module
 
 board = []
 
 for x in range(5):
     board.append(["O"] * 5)
 
-
+    
+# Printing board using a list
 def print_board(board):
     for row in board:
         print " ".join(row)
 
-
-print "Let's play Battleship!"
+print "Welcome! Let's play Battleship!"
 print_board(board)
 
-
+# Find a random location of the ocean
 def random_row(board):
     return randint(0, len(board) - 1)
 
@@ -22,12 +22,11 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
-
+# Assign the random place to ship
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
+# Create a 4 turn loop
 for turn in range(4):
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
